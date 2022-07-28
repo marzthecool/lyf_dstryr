eed410224a41ceec74335aeaf7f46fbed89e1065
Mary Laura Carmona Martínez <70768750+marzthecool@users.noreply.github.com>
2022-07-28 13:19:39 -0500

    sending for debugging

--- lyfDestryr.exs
+++ lyfDestryr.exs
@@ -1,18 +1,43 @@
 defmodule LyfDestryr do
+  @aad "AES256GCM"
 
-
-  def hashKey(str) do
-    :crypto.hash(:sha256,str)
-    |> Base.encode16() \
-    |> String.downcase()
-
+  @doc """
+  Generates a random base64 encoded secret key.
+  """
+  def generate_secret do
+    :crypto.strong_rand_bytes(16)
+    |> :base64.encode
   end
+
+
+  @doc """
+  Takes in two arguments: the value to be encrypted and the secret key to encrypt it with.
+  """
+  def do_encrypt(val, key) do
+    # using the Advanced Encryption Standard Galois Counter Mode
+    mode = :aes_gcm
+    # converts the key to binary
+    secret_key = :base64.decode(key)
+    # generates an initialization vector
+    iv = :crypto.strong_rand_bytes(16)
+    # ???
+    {ciphertext, ciphertag} = :crypto.block_encrypt(mode, secret_key, {@aad, to_string(val), 16})
+    iv <> ciphertag <> ciphertext
+  end
+
+
+  # def hashKey(str) do
+  #   :crypto.hash(:sha256,str)
+  #   |> Base.encode16() \
+  #   |> String.downcase()
+  # end
+
 
   @doc """
   Sort the numbers found in each of the lines
   Keep the order of the lines as it is
   """
-  def sort_file_numbers(in_filename, out_filename) do
+  def encrypt_file(in_filename, out_filename) do
     new_text = in_filename
       # Read the file line by line
       |> File.stream!()
@@ -31,21 +56,12 @@ defmodule LyfDestryr do
   to a list, sorting, then converting back to a string
   """
   def sort_lines(line) do
+    secret = LyfDestryr.generate_secret
     line
     # Separate the tokens at the spaces. This creates a list
     |> String.split()
-
-    |> Enum.map(&str_to_bi(&1))
-
+    |> Enum.map(&do_encrypt(&1, secret))
     # Joint the strings in the list into a single string
     |> Enum.join(" ")
-
   end
-
-  def str_to_bi(line) do
-    line
-    # Convert string line its binary value
-    |> String.to_charlist()
-  end
-
 end

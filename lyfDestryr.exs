ecf9f974268c5d83f5f59641deee7ad52cedda27
Mary Laura Carmona Martínez <70768750+marzthecool@users.noreply.github.com>
2022-07-28 14:28:09 -0500

    dencrypt

--- lyfDestryr.exs
+++ lyfDestryr.exs
@@ -1,3 +1,5 @@
+# secret = LyfDestryr.generate_secret
+# LyfDestryr.do_decrypt("R0NklEhjgtHljd4U1bcPbHhdVzbJHiiFbC3IydA8Hj34JeMrypmoJbmJBw==", secret)
 defmodule LyfDestryr do
   @aad "AES256GCM"
 
@@ -21,16 +23,38 @@ defmodule LyfDestryr do
     # generates an initialization vector
     iv = :crypto.strong_rand_bytes(16)
     # ???
-    {ciphertext, ciphertag} = :crypto.block_encrypt(mode, secret_key, {@aad, to_string(val), 16})
+    {ciphertext, ciphertag} = :crypto.block_encrypt(mode, secret_key, iv, {@aad, to_string(val), 16})
     iv <> ciphertag <> ciphertext
+    |> :base64.encode
+  end
+
+  @doc """
+  Takes in an argument of the ciphertext and the secret key that was used to encrypt it.
+  """
+  def do_decrypt(ciphertext, key) do
+    mode = :aes_gcm
+    secret_key = :base64.decode(key)
+    ciphertext = :base64.decode(ciphertext)
+    <<iv::binary-16, tag::binary-16, ciphertext::binary>> = ciphertext
+    :crypto.block_decrypt(mode, secret_key, iv, {@aad, ciphertext, tag})
   end
 
 
-  # def hashKey(str) do
-  #   :crypto.hash(:sha256,str)
-  #   |> Base.encode16() \
-  #   |> String.downcase()
-  # end
+  @doc """
+  Sort the numbers represented as a string, by converting them
+  to a list, sorting, then converting back to a string
+  """
+  def sort_lines(line, mode) do
+    secret = LyfDestryr.generate_secret
+    # Separate the tokens at the spaces. This creates a list
+    # word_list = String.split(line)
+    cond do
+      mode == "encrypt" ->
+        do_encrypt(line, secret)
+      mode == "decrypt" ->
+        do_decrypt(line, secret)
+    end
+  end
 
 
   @doc """
@@ -44,24 +68,29 @@ defmodule LyfDestryr do
       # Remove trailing spaces around each line
       |> Enum.map(&String.trim/1)
       # Apply the function below to each line
-      |> Enum.map(&sort_lines(&1))
+      |> Enum.map(&sort_lines(&1, "encrypt"))
       # Join all the lines into a single string separated with New Line characters
       |> Enum.join("\n")
     # Store the final string into the file
     File.write(out_filename, new_text)
   end
 
+
   @doc """
-  Sort the numbers represented as a string, by converting them
-  to a list, sorting, then converting back to a string
+  Sort the numbers found in each of the lines
+  Keep the order of the lines as it is
   """
-  def sort_lines(line) do
-    secret = LyfDestryr.generate_secret
-    line
-    # Separate the tokens at the spaces. This creates a list
-    |> String.split()
-    |> Enum.map(&do_encrypt(&1, secret))
-    # Joint the strings in the list into a single string
-    |> Enum.join(" ")
+  def decrypt_file(in_filename, out_filename) do
+    new_text = in_filename
+      # Read the file line by line
+      |> File.stream!()
+      # Remove trailing spaces around each line
+      |> Enum.map(&String.trim/1)
+      # Apply the function below to each line
+      |> Enum.map(&sort_lines(&1, "decrypt"))
+      # Join all the lines into a single string separated with New Line characters
+      |> Enum.join("\n")
+    # Store the final string into the file
+    File.write(out_filename, new_text)
   end
 end
